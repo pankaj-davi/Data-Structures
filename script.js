9492a287a36e0e428ab98da19f29167102159e7c
pankaj-davi <menariapankaj17@gmail.com>
2021-09-06 10:37:52 +0530

    Inital commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Pice of data - val
+// refterance to next node  - next
+// initialized next  null
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // Adding a new node to the end of the Linked List!
+    push(val){
+        
+    }
+}
